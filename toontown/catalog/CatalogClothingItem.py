3c3307374ca1db9d0ac3df352abc744a12b4f687
Nayla Hanegan <naylahanegan@gmail.com>
2023-06-30 17:26:57 -0400

    gender neutral: clothes and accesseries?

--- toontown/catalog/CatalogClothingItem.py
+++ toontown/catalog/CatalogClothingItem.py
@@ -309,18 +309,10 @@ class CatalogClothingItem(CatalogItem.CatalogItem):
             return forBoys
 
     def forBoysOnly(self):
-        article = ClothingTypes[self.clothingType][CTArticle]
-        if article == ABoysShirt or article == ABoysShorts:
-            return 1
-        else:
-            return 0
+        return 0
 
     def forGirlsOnly(self):
-        article = ClothingTypes[self.clothingType][CTArticle]
-        if article == AGirlsShirt or article == AGirlsSkirt or article == AGirlsShorts:
-            return 1
-        else:
-            return 0
+        return 0
 
     def getPurchaseLimit(self):
         return 1
